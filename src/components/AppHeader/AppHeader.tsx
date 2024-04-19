f4bbe98b5a4dc6261e45fad27f5f4124e1e7608d
Sergey Nagornykh <serega1905@ukr.net>
2024-04-19 20:27:57 +0300

    :lipstick: update: AppHeader + dark mode

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,9 +1,29 @@
 import '@/components/AppHeader/AppHeader.scss'
+import IconMoon from '@/assets/icon-moon.svg'
+import IconSun from '@/assets/icon-sun.svg'
+import { useDarkMode } from 'usehooks-ts'
+import { useEffect } from 'react'
 
 function AppHeader() {
+  const { isDarkMode, toggle } = useDarkMode({
+    localStorageKey: 'darkMode',
+  })
+
+  useEffect(() => {
+    document.body.dataset.theme = isDarkMode ? 'dark' : 'light'
+  }, [isDarkMode])
+
   return (
-    <header>
-      <h1>App Header</h1>
+    <header className="app-header">
+      <h1 className="app-header__title">Todo</h1>
+
+      <button className="app-header__theme-toggle" onClick={toggle}>
+        {isDarkMode ? (
+          <img src={IconMoon} alt="" />
+        ) : (
+          <img src={IconSun} alt="" />
+        )}
+      </button>
     </header>
   )
 }
